fcf7c54ffff6779485973c771758c91dba452721
gpaulissen <gpaulissen@c1c61559-3a1e-0410-b1f8-a6e2dc18b5fa>
2008-01-06 10:31:48 +0000

    Adding bind variables

--- trunk/oradumper/src/lib/oradumper_int.c
+++ trunk/oradumper/src/lib/oradumper_int.c
@@ -42,13 +42,23 @@ static struct {
   /*@null@*/ char *def; /* default */
   /*@observer@*//*@null@*/ char *value; /* value */
 } opt[] = { 
-  { "userid", 1, "Oracle connect string", "scott/tiger", NULL },
+  { "userid", 0, "Oracle connect string", NULL, NULL }, /* userid may be NULL when oradumper is used as a library */
   { "sqlstmt", 1, "Select statement", NULL, NULL },
   { "arraysize", 1, "Array size", "10", NULL },
   { "dbug_options", 1, "DBUG options", "", NULL },
   { "nls_date_format", 1, "NLS date format", "yyyy-mm-dd hh24:mi:ss", NULL },
   { "nls_timestamp_format", 1, "NLS timestamp format", "yyyy-mm-dd hh24:mi:ss", NULL },
-  { "nls_numeric_characters", 1, "NLS numeric characters", ".,", NULL }
+  { "nls_numeric_characters", 1, "NLS numeric characters", ".,", NULL },
+  { "b1", 0, "Bind variable 1", NULL, NULL },
+  { "b2", 0, "Bind variable 2", NULL, NULL },
+  { "b3", 0, "Bind variable 3", NULL, NULL },
+  { "b4", 0, "Bind variable 4", NULL, NULL },
+  { "b5", 0, "Bind variable 5", NULL, NULL },
+  { "b6", 0, "Bind variable 6", NULL, NULL },
+  { "b7", 0, "Bind variable 7", NULL, NULL },
+  { "b8", 0, "Bind variable 8", NULL, NULL },
+  { "b9", 0, "Bind variable 9", NULL, NULL },
+  { "b10", 0, "Bind variable 10", NULL, NULL },
 };
 
 static
@@ -57,7 +67,7 @@ usage(void)
 {
   size_t i;
 
-  (void) fprintf( stderr, "\nUsage: oradumper [OPTION]...\n\nOPTION:\n");
+  (void) fprintf( stderr, "\nUsage: oradumper [OPTION=VALUE]...\n\nOPTION:\n");
   for (i = 0; i < sizeof(opt)/sizeof(opt[0]); i++)
     {
       (void) fprintf( stderr, "  %-30s  %s", opt[i].name, opt[i].desc);
@@ -90,7 +100,7 @@ process_options(const unsigned int length, const char **options)
 
       if (j == sizeof(opt)/sizeof(opt[0]))
 	{
-	  (void) fprintf(stderr, "\nERROR: Option %s unknown.\n", options[i]);
+	  (void) fprintf(stderr, "\nERROR: OPTION in OPTION=VALUE (%s) unknown.\n", options[i]);
 	  usage();
 	}
     }
@@ -127,6 +137,16 @@ get_option(const option_t option)
     case OPTION_NLS_DATE_FORMAT:
     case OPTION_NLS_TIMESTAMP_FORMAT:
     case OPTION_NLS_NUMERIC_CHARACTERS:
+    case OPTION_B1:
+    case OPTION_B2:
+    case OPTION_B3:
+    case OPTION_B4:
+    case OPTION_B5:
+    case OPTION_B6:
+    case OPTION_B7:
+    case OPTION_B8:
+    case OPTION_B9:
+    case OPTION_B10:
       return opt[option].value;
     }
 
@@ -138,7 +158,24 @@ get_option(const option_t option)
 int
 oradumper(const unsigned int length, const char **options)
 {
-  int status = 1;
+  typedef enum {
+    STEP_CONNECT = 0,
+    STEP_NLS_DATE_FORMAT,
+    STEP_NLS_TIMESTAMP_FORMAT,
+    STEP_NLS_NUMERIC_CHARACTERS,
+    STEP_ALLOCATE_DESCRIPTORS,
+    STEP_PARSE,
+    STEP_BIND_VARIABLE,
+    STEP_OPEN_CURSOR,
+
+    STEP_CLOSE_CURSOR,
+    STEP_DEALLOCATE_DESCRIPTORS
+
+#define STEP_MAX ((int) STEP_DEALLOCATE_DESCRIPTORS)
+  } step_t;
+  int step;
+
+  int status = OK;
 #define NLS_MAX_SIZE 100
   char nls_date_format_stmt[NLS_MAX_SIZE+1];
   char nls_timestamp_format_stmt[NLS_MAX_SIZE+1];
@@ -149,64 +186,110 @@ oradumper(const unsigned int length, const char **options)
   const char *nls_numeric_characters = get_option(OPTION_NLS_NUMERIC_CHARACTERS);
   const char *array_size = get_option(OPTION_ARRAYSIZE);
   const char *sqlstmt = get_option(OPTION_SQLSTMT);
+  unsigned int bind_variable_count = 0, bind_variable_nr;
+  char bind_variable_name[30+1] = "";
+  char *bind_variable_value;
 	  
   process_options(length, options);
 
   DBUG_INIT(get_option(OPTION_DBUG_OPTIONS), "oradumper");
 
-  do
+  for (step = (step_t) 0; step <= STEP_MAX && status == OK; step++)
     {
-      assert(userid != NULL);
+      switch((step_t) step)
+	{
+	case STEP_CONNECT:
+	  if (userid != NULL)
+	    {
+	      status = sql_connect(userid);
+	    }
+	  break;
 
-      /* Connect to the database. */
-      if (sql_connect(userid) != OK)
-	break;
+	case STEP_NLS_DATE_FORMAT:
+	  assert(nls_date_format != NULL);
 
-      assert(nls_date_format != NULL);
+	  (void) snprintf(nls_date_format_stmt,
+			  sizeof(nls_date_format_stmt),
+			  "ALTER SESSION SET NLS_DATE_FORMAT = '%s'",
+			  nls_date_format);
 
-      (void) snprintf(nls_date_format_stmt,
-		      sizeof(nls_date_format_stmt),
-		      "ALTER SESSION SET NLS_DATE_FORMAT = '%s'",
-		      nls_date_format);
+	  status = sql_execute_immediate(nls_date_format_stmt);
+	  break;
 
-      if (sql_execute_immediate(nls_date_format_stmt) != OK)
-	break;
+	case STEP_NLS_TIMESTAMP_FORMAT:
+	  assert(nls_timestamp_format != NULL);
 
-      assert(nls_timestamp_format != NULL);
+	  (void) snprintf(nls_timestamp_format_stmt,
+			  sizeof(nls_timestamp_format_stmt),
+			  "ALTER SESSION SET NLS_TIMESTAMP_FORMAT = '%s'",
+			  nls_timestamp_format);
 
-      (void) snprintf(nls_timestamp_format_stmt,
-		      sizeof(nls_timestamp_format_stmt),
-		      "ALTER SESSION SET NLS_TIMESTAMP_FORMAT = '%s'",
-		      nls_timestamp_format);
+	  status = sql_execute_immediate(nls_timestamp_format_stmt);
+	  break;
 
-      if (sql_execute_immediate(nls_timestamp_format_stmt) != OK)
-	break;
-
-      assert(nls_timestamp_format != NULL);
-
-      assert(nls_numeric_characters != NULL);
-
-      (void) snprintf(nls_numeric_characters_stmt,
-		      sizeof(nls_numeric_characters_stmt),
-		      "ALTER SESSION SET NLS_NUMERIC_CHARACTERS = '%s'",
-		      nls_numeric_characters);
+	case STEP_NLS_NUMERIC_CHARACTERS:
+	  assert(nls_numeric_characters != NULL);
+	
+	  (void) snprintf(nls_numeric_characters_stmt,
+			  sizeof(nls_numeric_characters_stmt),
+			  "ALTER SESSION SET NLS_NUMERIC_CHARACTERS = '%s'",
+			  nls_numeric_characters);
       
-      if (sql_execute_immediate(nls_numeric_characters) != OK)
-	break;
+	  status = sql_execute_immediate(nls_numeric_characters);
+	  break;
 
-      assert(array_size != NULL);
+	case STEP_ALLOCATE_DESCRIPTORS:
+	  assert(array_size != NULL);
 
-      if (sql_allocate_descriptors((unsigned int) atoi(array_size)) != OK)
-	break;
+	  status = sql_allocate_descriptors((unsigned int) atoi(array_size));
+	  break;
 
-      assert(sqlstmt != NULL);
+	case STEP_PARSE:
+	  assert(sqlstmt != NULL);
 
-      if (sql_parse(sqlstmt) != OK)
-	break;
+	  status = sql_parse(sqlstmt);
+	  break;
 
-      status = 0;
+	case STEP_BIND_VARIABLE:
+	  if ((status = sql_bind_variable_count(&bind_variable_count)) != OK)
+	    break;
+
+	  for (bind_variable_nr = 0;
+	       bind_variable_nr < bind_variable_count && bind_variable_nr < MAX_BIND_VARIABLES;
+	       bind_variable_nr++)
+	    {
+	      if ((status = sql_bind_variable_name(bind_variable_nr + 1,
+						   sizeof(bind_variable_name),
+						   bind_variable_name)) != OK)
+		break;
+
+	      bind_variable_value = get_option((option_t)((unsigned int)OPTION_B1 + bind_variable_nr));
+
+	      DBUG_PRINT("info",
+			 ("bind variable %u has name %s and value %s",
+			  bind_variable_nr + 1,
+			  bind_variable_name,
+			  bind_variable_value));
+
+	      if ((status = sql_bind_variable(bind_variable_nr + 1, bind_variable_value)) != OK)
+		break;
+	    }
+	  break;
+
+	case STEP_OPEN_CURSOR:
+	  status = sql_open_cursor();
+	  break;
+
+	  /**/
+	case STEP_CLOSE_CURSOR:
+	  status = sql_close_cursor();
+	  break;
+
+	case STEP_DEALLOCATE_DESCRIPTORS:
+	  status = sql_deallocate_descriptors();
+	  break;
+	}
     }
-  while (1 != 1);
 
   DBUG_DONE();
 
