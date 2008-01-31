df28a90854c8b92e0ff1ad27171116147d1c325c
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2008-01-31 21:56:58 +0000

    Using check unit test framework

--- test/oradumper_check.c
+++ test/oradumper_check.c
@@ -1,14 +1,88 @@
-#include <stdlib.h>
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#if HAVE_CHECK_H
 #include <check.h>
+#endif
+
+#if HAVE_STDBOOL_H
+#include <stdbool.h>
+#else
+typedef int bool;
+#define false 0
+#define true 1
+#endif
+
+/* should always be there */
+#include <stdio.h>
+
+#if HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#if HAVE_SYS_ERRNO_H
+#include <sys/errno.h>
+#endif
+
 #include "../src/lib/oradumper.h"
 #include "../src/lib/oradumper_int.h"
 
-START_TEST (test_sizes)
+/* include dmalloc as last one */
+#ifdef WITH_DMALLOC
+#include <dmalloc.h>
+#endif
+
+static bool cmp_files(const char *file1, const char *file2)
 {
-  fail_unless (sizeof(value_name_t) == 31, 
-	       "Size of value_name_t must be 31.");
-  fail_unless (sizeof(character_set_name_t) == 21,
-	       "Size of character_set_name_t must be 21.");
+  FILE *fin1 = NULL, *fin2 = NULL;
+  bool result = false;
+  char line1[1000+1], line2[1000+1];
+
+  if ((fin1 = fopen(file1, "r")) != NULL &&
+      (fin2 = fopen(file2, "r")) != NULL)
+    {
+      result = true;
+      while (fgets(line1, sizeof(line1), fin1) != NULL &&
+	     fgets(line2, sizeof(line2), fin2) != NULL) {
+	/* each line read should be of equal size and equal content */
+	if (strcmp(line1, line2) != 0) {
+	  result = false;
+	  break;
+	}
+      }
+    }
+
+  if (fin1 != NULL) {
+    (void) fclose(fin1);
+  }
+  if (fin2 != NULL) {
+    (void) fclose(fin2);
+  }
+  
+  return result;
+}
+
+START_TEST(test_sizes)
+{
+  fail_unless(sizeof(value_name_t) > 30, NULL);
+  fail_unless(sizeof(character_set_name_t) >= 20, NULL);
+}
+END_TEST
+
+START_TEST(test_usage)
+{
+  const char usage_txt[] = "usage.txt";
+  const char usage_txt_ref[] = "usage.txt.ref";
+  FILE *fout = fopen(usage_txt, "w");
+
+  fail_if(fout == NULL, "File '%s' could not be opened for writing: %s", usage_txt, strerror(errno));
+
+  oradumper_usage(fout);
+
+  fail_if(fclose(fout) != 0, "File '%s' could not be closed: %s", usage_txt, strerror(errno));
+  fail_if(cmp_files(usage_txt, usage_txt_ref) == false,
+	  "Files '%s' and '%s' are not equal", usage_txt, usage_txt_ref);
 }
 END_TEST
 
@@ -20,6 +94,7 @@ options_suite (void)
   /* Core test case */
   TCase *tc_interface = tcase_create ("Interface");
   tcase_add_test (tc_interface, test_sizes);
+  tcase_add_test (tc_interface, test_usage);
   suite_add_tcase (s, tc_interface);
 
   return s;
