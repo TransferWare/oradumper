61a3eeff815ce395b07eaf9c918e22fa235b3fb7
gpaulissen <gpaulissen@c1c61559-3a1e-0410-b1f8-a6e2dc18b5fa>
2018-12-06 17:10:44 +0000

    Testing with unicode

--- trunk/oradumper/src/lib/oradumper.h
+++ trunk/oradumper/src/lib/oradumper.h
@@ -1,14 +1,16 @@
 #ifndef ORADUMPER_H
 #define ORADUMPER_H
 
+#define TRANSFERWARE_BUG_67 1
+
 extern
 /*@null@*/ /*@observer@*/
 char *
 oradumper(const unsigned int nr_arguments,
-	  const char **arguments,
-	  const int disconnect,
-	  const size_t error_msg_size,
-	  /*@out@*/ char *error_msg,
-	  /*@out@*/ unsigned int *row_count);
+    const char **arguments,
+    const int disconnect,
+    const size_t error_msg_size,
+    /*@out@*/ char *error_msg,
+    /*@out@*/ unsigned int *row_count);
 
 #endif
